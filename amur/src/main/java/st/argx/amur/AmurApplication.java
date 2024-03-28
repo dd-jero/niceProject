f2722bf152e16e26e6b47f26ee7f4d19cdd5512b
sihyunjojo <si4018@naver.com>
2024-03-28 21:08:25 +0900

    add: 프로젝트 시작

---
+++ amur/src/main/java/st/argx/amur/AmurApplication.java
@@ -0,0 +1,13 @@
+package st.argx.amur;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AmurApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AmurApplication.class, args);
+	}
+
+}
---
+++ amur/src/test/java/st/argx/amur/AmurApplicationTests.java
@@ -0,0 +1,13 @@
+package st.argx.amur;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AmurApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
